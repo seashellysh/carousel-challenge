3f678cb0b2b8a918daae477806e7a56785fd758c
Helly Shah <hellyshah@google.com>
2023-03-01 17:01:29 -0500

    Add starter code for carousel challenge

---
+++ carousel.js
@@ -0,0 +1,28 @@
+// STEP 1: Select all slides and store them in the variable <slides>
+// using document.querySelectorAll()
+
+// Loop through slides and set each slides translateX.
+slides.forEach((slide, indx) => {
+    slide.style.transform = `translateX(${indx * 100}%)`;
+});
+
+// STEP 2: Select next slide button
+
+// Current slide counter
+let curSlide = 0;
+
+// STEP 3: Calculate the maximum number of slides
+
+// Add event listener and navigation functionality
+nextSlide.addEventListener("click", function () {
+    // STEP 4: Check if current slide is the last and reset current slide
+
+    // STEP 5: Loop through the slides and move each slide by -100% by using
+    // `translateX(${100 * (indx - curSlide)}%)`
+});
+
+// Select previous slide button
+const prevSlide = document.querySelector(".btn-prev");
+
+// STEP 5: Add event listener and navigation functionality.
+// HINT: Use the code you wrote to add the functionality to the next slide button. 
